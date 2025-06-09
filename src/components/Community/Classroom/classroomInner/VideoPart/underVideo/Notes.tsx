6e6a3a93a4cf5b91b119729714fee2eb4bae97e2
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2025-06-10 01:23:32 +0300

    Update Notes and OverView components with improved styling
    - Added padding to the Notes component for better spacing.
    - Updated OverView component to include padding and removed unnecessary elements for a cleaner layout.

--- src/components/Community/Classroom/classroomInner/VideoPart/underVideo/Notes.tsx
+++ src/components/Community/Classroom/classroomInner/VideoPart/underVideo/Notes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Notes = () => {
-  return <div>Notes</div>;
+  return <div className="p-8">Notes</div>;
 };
 
 export default Notes;
--- src/components/Community/Classroom/classroomInner/VideoPart/underVideo/OverView.tsx
+++ src/components/Community/Classroom/classroomInner/VideoPart/underVideo/OverView.tsx
@@ -1,31 +1,9 @@
 import React from "react";
-import { FaStar } from "react-icons/fa";
 
 const OverView = ({ description }: { description?: string }) => {
   return (
-    <div className="">
+    <div className="p-8">
       <p>{description || "No description provided."}</p>
-      <div className="flex gap-16 flex-wrap pt-8">
-        <div className="flex flex-col items-start gap-1">
-          <div className="flex items-center justify-center gap-4">
-            <p className="h-[20px]">4.8</p>
-            <FaStar fontSize={20} className="text-yellow-500" />
-          </div>
-          <p className="p-muted">868 ratings</p>
-        </div>
-        <div className="flex flex-col items-start gap-1">
-          <div className="flex ">
-            <p className="h-[20px]">10.896</p>
-          </div>
-          <p className="p-muted">students</p>
-        </div>
-        <div className="flex flex-col items-start gap-1">
-          <div className="flex ">
-            <p className="h-[20px]">21.5 hours</p>
-          </div>
-          <p className="p-muted">total</p>
-        </div>
-      </div>
     </div>
   );
 };
