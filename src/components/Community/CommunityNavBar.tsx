44f898185c4284431141fdd282a6f41fa242c20e
Youssif Abdelrazek <youssifh488@gmail.com>
2025-06-09 20:57:15 +0300

    Adding admin to community's navbar

--- src/components/Community/CommunityNavBar.tsx
+++ src/components/Community/CommunityNavBar.tsx
@@ -5,77 +5,39 @@ import { usePathname, useParams } from 'next/navigation'
 export default function CommunityNavBar() {
   const { community } = useParams()
   const pathname = usePathname()
+  const currentPage = pathname.split('/').pop() || ''
+
+  const navItems = [
+    { name: 'Forum', path: '' },
+    { name: 'Classroom', path: 'classroom' },
+    { name: 'Calendar', path: 'calendar' },
+    { name: 'Leaderboards', path: 'leaderboards' },
+    { name: 'Members', path: 'members' },
+    { name: 'About', path: 'about' },
+    { name: 'Settings', path: 'settings' },
+    { name: 'Admin', path: 'admin' },
+  ]
 
   return (
     <div className="flex justify-evenly bg-card rounded-md py-4 w-full">
-      <Link
-        href={`/c/${community}`}
-        className={`cursor-pointer relative before:absolute before:w-0 before:h-1 before:bg-accent before:-bottom-4 hover:before:w-full before:transition-all before:duration-700 ${
-          pathname.split('/').length === 2 ? 'text-accent before:w-full' : ''
-        }`}
-      >
-        Forum
-      </Link>
-      <Link
-        href={`/c/${community}/classroom`}
-        className={`cursor-pointer relative before:absolute before:w-0 before:h-1 before:bg-accent before:-bottom-4 hover:before:w-full before:transition-all before:duration-700 ${
-          pathname.split('/').at(-1) === 'classroom'
-            ? 'text-accent before:w-full'
-            : ''
-        }`}
-      >
-        Classroom
-      </Link>
-      <Link
-        href={`/c/${community}/calendar`}
-        className={`cursor-pointer relative before:absolute before:w-0 before:h-1 before:bg-accent before:-bottom-4 hover:before:w-full before:transition-all before:duration-700 ${
-          pathname.split('/').at(-1) === 'calendar'
-            ? 'text-accent before:w-full'
-            : ''
-        }`}
-      >
-        Calendar
-      </Link>
-      <Link
-        href={`/c/${community}/leaderboards`}
-        className={`cursor-pointer relative before:absolute before:w-0 before:h-1 before:bg-accent before:-bottom-4 hover:before:w-full before:transition-all before:duration-700 ${
-          pathname.split('/').at(-1) === 'leaderboards'
-            ? 'text-accent before:w-full'
-            : ''
-        }`}
-      >
-        Leaderboards
-      </Link>
-      <Link
-        href={`/c/${community}/members`}
-        className={`cursor-pointer relative before:absolute before:w-0 before:h-1 before:bg-accent before:-bottom-4 hover:before:w-full before:transition-all before:duration-700 ${
-          pathname.split('/').at(-1) === 'members'
-            ? 'text-accent before:w-full'
-            : ''
-        }`}
-      >
-        Members
-      </Link>
-      <Link
-        href={`/c/${community}/about`}
-        className={`cursor-pointer relative before:absolute before:w-0 before:h-1 before:bg-accent before:-bottom-4 hover:before:w-full before:transition-all before:duration-700 ${
-          pathname.split('/').at(-1) === 'about'
-            ? 'text-accent before:w-full'
-            : ''
-        }`}
-      >
-        About
-      </Link>
-      <Link
-        href={`/c/${community}/settings`}
-        className={`cursor-pointer relative before:absolute before:w-0 before:h-1 before:bg-accent before:-bottom-4 hover:before:w-full before:transition-all before:duration-700 ${
-          pathname.split('/').at(-1) === 'settings'
-            ? 'text-accent before:w-full'
-            : ''
-        }`}
-      >
-        Settings
-      </Link>
+      {navItems.map((item) => {
+        const href = `/c/${community}${item.path ? `/${item.path}` : ''}`
+        const isActive =
+          (item.path === '' && pathname === href) ||
+          (item.path !== '' && currentPage === item.path)
+
+        return (
+          <Link
+            key={item.path}
+            href={href}
+            className={`cursor-pointer relative before:absolute before:w-0 before:h-1 before:bg-accent before:-bottom-4 hover:before:w-full before:transition-all before:duration-700 ${
+              isActive ? 'text-accent before:w-full' : ''
+            }`}
+          >
+            {item.name}
+          </Link>
+        )
+      })}
     </div>
   )
 }
